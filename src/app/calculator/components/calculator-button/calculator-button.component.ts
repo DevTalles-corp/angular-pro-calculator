346b2ca86a4495e82aa358654708b0dec3a4e10b
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-23 09:45:13 -0400

    Refactor calculator button component styles and behavior

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -6,6 +6,7 @@ import {
   input,
   OnInit,
   output,
+  signal,
   viewChild,
   ViewEncapsulation,
 } from '@angular/core';
@@ -25,6 +26,8 @@ import {
   // encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButtonComponent {
+  public isPressed = signal(false);
+
   public onClick = output<string>();
   public contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
 
@@ -54,5 +57,19 @@ export class CalculatorButtonComponent {
     const value = this.contentValue()!.nativeElement.innerText;
 
     this.onClick.emit(value.trim());
+  }
+
+  public keyboardPressedStyle(key: string) {
+    if (!this.contentValue()) return;
+
+    const value = this.contentValue()!.nativeElement.innerText;
+
+    if (value !== key) return;
+
+    this.isPressed.set(true);
+
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100);
   }
 }
--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  viewChildren,
+} from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
 
 @Component({
@@ -7,6 +11,9 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
   // styles: `
   //   // .is-command {
   //   //   @apply bg-indigo-700 bg-opacity-20;
@@ -14,7 +21,29 @@ import { CalculatorButtonComponent } from '../calculator-button/calculator-butto
   // `,
 })
 export class CalculatorComponent {
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
   handleClick(key: string) {
     console.log({ key });
+  }
+
+  // @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const keyEquivalents: Record<string, string> = {
+      Escape: 'C',
+      Clear: 'C',
+      '*': 'x',
+      '/': '÷',
+      Enter: '=',
+    };
+
+    const key = event.key;
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue);
+
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyle(keyValue);
+    });
   }
 }
