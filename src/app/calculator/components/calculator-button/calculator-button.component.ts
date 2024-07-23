17c7f74336a0b32c45b7183e0cd33e4aa3e4a8ba
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-23 10:04:24 -0400

    Refactor calculator button component styles to use dynamic sizing based on isDoubleSize() function

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -19,7 +19,9 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
   styleUrl: './calculator-button.component.css',
   host: {
-    class: 'w-1/4 border-r border-b border-indigo-400',
+    class: 'border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
     // attribute: 'hola',
     // 'data-size': 'XL',
   },
@@ -45,9 +47,9 @@ export class CalculatorButtonComponent {
   //   return this.isCommand();
   // }
 
-  @HostBinding('class.w-2/4') get commandStyle() {
-    return this.isDoubleSize();
-  }
+  // @HostBinding('class.w-2/4') get commandStyle() {
+  //   return this.isDoubleSize();
+  // }
 
   handleClick() {
     if (!this.contentValue()?.nativeElement) {
