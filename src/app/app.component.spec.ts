26b3619fccdad016a9822a54a89ae43a67ae3c83
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-24 08:47:04 -0400

    Refactor calculator component to use computed properties for result text

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,32 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have the 'zoneless-calculator' title`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('zoneless-calculator');
-  // });
+  it('should be 3', () => {
+    // A = Arrange
+    const num1 = 1;
+    const num2 = 2;
+
+    // A = Act
+    const result = num1 + num2;
+
+    // A = Assert
+    expect(result).toBe(3);
+  });
+
+  it(`should have the 'zoneless-calculator' title`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('zoneless-calculator');
+  });
 
   // it('should render title', () => {
   //   const fixture = TestBed.createComponent(AppComponent);
   //   fixture.detectChanges();
+
   //   const compiled = fixture.nativeElement as HTMLElement;
+
+  //   // console.log(compiled);
+
   //   expect(compiled.querySelector('h1')?.textContent).toContain(
   //     'Hello, zoneless-calculator'
   //   );
