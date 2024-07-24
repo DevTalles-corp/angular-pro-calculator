8283047eb73ef881fa73ba0352a7c6d28102d426
Fernando Herrera <fernando.herrera85@gmail.com>
2024-07-24 13:42:32 -0400

    Refactor calculator button component styles to use dynamic sizing based on isDoubleSize() function

---
+++ src/app/calculator/components/calculator-button/calculator-button.component.spec.ts
@@ -0,0 +1,42 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CalculatorButtonComponent } from './calculator-button.component';
+import { Component } from '@angular/core';
+
+describe('CalculatorButtonComponent', () => {
+  let fixture: ComponentFixture<CalculatorButtonComponent>;
+  let compiled: HTMLElement;
+  let component: CalculatorButtonComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CalculatorButtonComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CalculatorButtonComponent);
+    compiled = fixture.nativeElement as HTMLElement;
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+  });
+
+  it('should create the app', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should apply w-1/4 doubleSize is false', () => {
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+
+    expect(hostCssClasses).toContain('w-1/4');
+    expect(component.isDoubleSize()).toBeFalse();
+  });
+
+  it('should apply w-2/4 doubleSize is true', () => {
+    fixture.componentRef.setInput('isDoubleSize', true);
+    fixture.detectChanges();
+
+    const hostCssClasses: string[] = compiled.classList.value.split(' ');
+
+    expect(hostCssClasses).toContain('w-2/4');
+    expect(component.isDoubleSize()).toBeTrue();
+  });
+});
