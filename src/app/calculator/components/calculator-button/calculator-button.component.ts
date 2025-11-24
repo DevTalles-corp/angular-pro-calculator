167a80c095581ea1271a02b516a61bc492fce911
Fernando Herrera <fernando.herrera85@gmail.com>
2025-11-24 14:51:57 -0500

    chore: Upgrade Angular dependencies to version 19 and update package-lock.json

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -12,20 +12,18 @@ import {
 } from '@angular/core';
 
 @Component({
-  selector: 'calculator-button',
-  standalone: true,
-  imports: [],
-  templateUrl: './calculator-button.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  styleUrl: './calculator-button.component.css',
-  host: {
-    class: 'border-r border-b border-indigo-400',
-    '[class.w-2/4]': 'isDoubleSize()',
-    '[class.w-1/4]': '!isDoubleSize()',
-    // attribute: 'hola',
-    // 'data-size': 'XL',
-  },
-  // encapsulation: ViewEncapsulation.None,
+    selector: 'calculator-button',
+    imports: [],
+    templateUrl: './calculator-button.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    styleUrl: './calculator-button.component.css',
+    host: {
+        class: 'border-r border-b border-indigo-400',
+        '[class.w-2/4]': 'isDoubleSize()',
+        '[class.w-1/4]': '!isDoubleSize()',
+        // attribute: 'hola',
+        // 'data-size': 'XL',
+    }
 })
 export class CalculatorButtonComponent {
   public isPressed = signal(false);
--- src/app/calculator/views/calculator-view/calculator-view.component.ts
+++ src/app/calculator/views/calculator-view/calculator-view.component.ts
@@ -2,10 +2,9 @@ import { CalculatorComponent } from '@/calculator/components/calculator/calculat
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'calculator-view',
-  standalone: true,
-  imports: [CalculatorComponent],
-  templateUrl: './calculator-view.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'calculator-view',
+    imports: [CalculatorComponent],
+    templateUrl: './calculator-view.component.html',
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export default class CalculatorViewComponent {}
