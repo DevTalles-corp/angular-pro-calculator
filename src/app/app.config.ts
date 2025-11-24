a8bd572af18714c3add5aac24c8abbbd64fb1fe4
Fernando Herrera <fernando.herrera85@gmail.com>
2025-11-24 14:58:35 -0500

    chore: Enable Zoneless Change Detection in app configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import {
   ApplicationConfig,
-  // provideZonelessChangeDetection,
+  provideZonelessChangeDetection,
   // provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
@@ -10,8 +10,7 @@ import { routes } from './app.routes';
 export const appConfig: ApplicationConfig = {
   providers: [
     // provideZoneChangeDetection({ eventCoalescing: true }),
-    // Ya no es necesario porque Angular 21 ya tiene Zoneless Change Detection por defecto
-    // provideZonelessChangeDetection(),
+    provideZonelessChangeDetection(),
     provideRouter(routes),
   ],
 };
